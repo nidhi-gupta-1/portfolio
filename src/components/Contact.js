7e6d7ee5abd28a5f512cdea39931c354330b374c
nidhi <nkgpta1@gmail.com>
2024-06-22 18:55:45 +0530

    bugfix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,9 +51,9 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a href='' className="text-indigo-400 leading-relaxed">
+              <h2 className="text-indigo-400 leading-relaxed">
                 work.nidhigupta@gmail.com
-              </a>
+              </h2>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
