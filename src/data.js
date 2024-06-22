cf8ef2bede7eba40b716699dc5d8b245e0d330ea
nidhi <nkgpta1@gmail.com>
2024-06-22 18:32:38 +0530

    deploy

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image:
+      "https://s3-alpha.figma.com/hub/file/2718247115/30438230-ba72-4318-8cb5-6865767ef82e-cover.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = [
+  "ReactJs",
+  "Redux",
+  "JavaScript",
+  "TypeScript",
+  "Es6 Standards",
+  "NextJs",
+  "Tailwind Css",
+  "Css3",
+  "HTML5",
+  "Material-UI",
+  "HighCharts",
+  "Ag-Grid",
+  "Axios",
+  "Restful API",
+  "Postman",
+  "Swagger",
+  "Jest",
+  "React Testing Liberary",
+  "Vitest",
+  "Vite",
+  "Webpack",
+  "Jenkins",
+  "NodeJS",
+  "ExpressJs",
+  "PostgreSQL",
+  "MySQL",
+  "JWT Authentication",
+  "Microservices",
+  "Micro Frontend",
+  "Git",
+  "Github",
+  "GitLab",
+  "Bitbucket",
+  "JIRA",
+  "Confluence",
+  "Agile",
+];
