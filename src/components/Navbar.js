81c52da57ec27177149020c7c881e8b14c3d757b
nidhi <nkgpta1@gmail.com>
2024-06-30 02:12:46 +0530

    new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,11 @@ export default function Navbar({theme, setTheme}) {
     <header className="dark:bg-gray-800 bg-gray-100 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
         <div className="title-font font-medium mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="ml-3 text-2xl">
             Nidhi Gupta
           </a>
         </div>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-lg justify-center">
           <a href="#projects" className="px-2 dark:hover:bg-gray-700 hover:bg-gray-200 rounded">
             Past Work
           </a>
@@ -27,7 +27,7 @@ export default function Navbar({theme, setTheme}) {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center dark:bg-gray-800 border-0 py-1 px-3 focus:outline-none dark:hover:bg-gray-700 hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center dark:bg-gray-800 border-0 py-1 px-3 focus:outline-none dark:hover:bg-gray-700 hover:bg-gray-200 rounded text-lg mt-4 md:mt-0"
           >
             Connect
             <ChatAltIcon className="w-8 h-8 ml-1 text-green-300" />
